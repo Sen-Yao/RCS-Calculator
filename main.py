bbde1f729a4cdf366fb21f18e7fd0b5cac94ba3a
Sen-Yao <1574653569@qq.com>
2023-12-26 11:47:10 +0800

    Add pbar for training
    Remove parameter "unit"

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+import tqdm
+import warnings
 
 from info_io import load_E_field, single_RCS_output, load_RCS, rcs_to_dB
 from RCS_Table import RCS_Table
@@ -11,23 +13,21 @@ def main():
     :return:
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--unit', '-u', default='degree', type=str, help='angle unit')
     parser.add_argument('--path', default='data', type=str, help='path to data folder')
     parser.add_argument('--Ei', '-E', default=1, type=float, help='Mode of incident electric field vector')
     parser.add_argument('--R', '-R', default=1, type=float, help='Far field range')
     args = parser.parse_args()
 
-    print('Parameter:',
-          '\nAngle unit =', args.unit,
+    print('\n\nParameter:',
           '\nPath to data folder =', args.path,
           '\nMode of incident electric field vector =', args.Ei,
           '\nFar field range =', args.R,
-          '\n\nResult:')
+          '\n\n Start Calculating...')
 
     RCS_Table_list = []
     for root, directories, files in os.walk(args.path):
         # Detect E_field txt
-        for filename in files:
+        for filename in tqdm.tqdm(files):
             if filename[0:2] == 'E_' and filename[-4:] == '.txt':
                 Table = RCS_Table(args.R, args.Ei)
                 Table.E_table, Table.sample_num, Table.frequency = load_E_field(os.path.join(args.path, filename))
@@ -45,8 +45,11 @@ def main():
                     if Table.frequency_str == filename[4:-4]:
                         Table.CST_table = load_RCS(os.path.join(args.path, filename))
                         Table.check_single_RCS()
+            else:
+                warnings.warn('Detect unknown file "'+filename+'" in your data folder'+str(args.path))
 
     RCS_Table_list.sort(key=lambda x: x.frequency)
+    print('\n\nResult:')
     for Table in RCS_Table_list:
         print('When Frequency =', Table.frequency_str + 'Hz, the total RCS =', Table.total_RCS,
               'or', rcs_to_dB(Table.total_RCS), 'dB')
